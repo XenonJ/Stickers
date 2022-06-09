fb481e9a4c445897a95e2c109a17dbc7e6fe0d7b
蔡晨 <151370772@qq.com>
2022-06-09 20:35:56 +0800

    1

--- front/src/pages/myself.vue
+++ front/src/pages/myself.vue
@@ -14,13 +14,13 @@
             <el-avatar
               shape="circle"
               :size="size"
-              :src="post.header"
+              :src="this.profile_url"
               class="header-img"
               @click.native="addRoute1"
             ></el-avatar>
             <div class="post-info">
-              <span class="author-name">{{ post.authorName }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<el-link icon="el-icon-edit">编辑</el-link></span>
-              <span class="author-time">{{ post.id }}</span>
+              <span class="author-name">{{ this.user_name }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<el-link icon="el-icon-edit" @click="Editmyself">编辑</el-link></span>
+              <span class="author-time">{{ this.user_id }}</span>
             </div>
 
 
@@ -29,7 +29,7 @@
                 size="medium"
                 round
                 class="el-icon-close"
-                @click="addRoute2"
+                @click="open2"
               ></el-button>
             </div>
           </div>
@@ -75,22 +75,56 @@
 </template>
 
 <script>
+import Axios from "axios";
+import { axiosInstance } from "../boot/axios.js";
+const clickoutside = {
+  // 初始化指令
+  bind(el, binding, node) {
+    function documentHandler(e) {
+      // 这里判断点击的元素是否是本身，是本身，则返回
+      if (el.contains(e.target)) {
+        return false;
+      }
+      // 判断指令中是否绑定了函数
+      if (binding.expression) {
+        // 如果绑定了函数 则调用那个函数，此处binding.value就是handleClose方法
+        binding.value(e);
+      }
+    }
+    // 给当前元素绑定个私有变量，方便在unbind中可以解除事件监听
+    el.vueClickOutside = documentHandler;
+    document.addEventListener("click", documentHandler);
+  },
+  update() {},
+  unbind(el, binding) {
+    // 解除事件监听
+    document.removeEventListener("click", el.vueClickOutside);
+    delete el.vueClickOutside;
+  },
+};
+
 export default {
   name: "myself",
+  props: {
+    token: String,
+    user_id: Number,
+    user_name: String,
+    profile_url: String,
+    post_id:String,
+    post_time:String,
+  },
   data() {
     return {
-      post: {
-        header: "",
-        authorName: "zzh",
-        id: "00000000",
-        likes: "28",
-        flg: true,
-        pictureUrl:
-          "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
-        previewUrl: [
-          "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
-        ],
-      },
+      user: [],
+      value1: true,
+      btnShow: false,
+      index: "0",
+      post_flg: true,
+      to: "",
+      toId: -1,
+      post: [],
+      replyComment: "",
+      
       activities: [{
           content: '上传了帖子',
           timestamp: '2022-01-01 20:46',
@@ -109,28 +143,75 @@ export default {
           timestamp: '2018-04-03 20:46',
           size: 'large',
         }],
-      value2: [0, 360],
-      value3: 1,
-      textarea: "",
-      input1: "",
-      input2: "",
-      options: [
-        {
-          label: "/static/img/rel/right.png",
-          value1: "0",
-        },
-        {
-          label: "/static/img/rel/left.png",
-          value1: "1",
-        },
-      ],
+ 
     };
   },
   methods: {
+    Refresh: function () {
+      this.Refresh();
+
+      Axios.get("http://127.0.0.1:8000/pages/user_detail/", {
+        params: {
+          token: this.token,
+        },
+      })
+        .then((res) => {
+          console.log(res);
+          this.post = res.data["posts"];
+          this.user = res.data["user"];
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    Editmyself(){
+      Axios.post("http://127.0.0.1:8000/users/show_yourself/", {
+          params: {
+            show_yourself: this.show_yourself,
+            token: this.token,
+          },
+        });
+    },
+    dateStr(date) {
+      //获取js 时间戳
+      var time = new Date().getTime();
+      //去掉 js 时间戳后三位，与php 时间戳保持一致
+      time = parseInt((time - date) / 1000);
+      //存储转换值
+      var s;
+      if (time < 60 * 10) {
+        //十分钟内
+        return "刚刚";
+      } else if (time < 60 * 60 && time >= 60 * 10) {
+        //超过十分钟少于1小时
+        s = Math.floor(time / 60);
+        return s + "分钟前";
+      } else if (time < 60 * 60 * 24 && time >= 60 * 60) {
+        //超过1小时少于24小时
+        s = Math.floor(time / 60 / 60);
+        return s + "小时前";
+      } else if (time < 60 * 60 * 24 * 30 && time >= 60 * 60 * 24) {
+        //超过1天少于30天内
+        s = Math.floor(time / 60 / 60 / 24);
+        return s + "天前";
+      } else {
+        //超过30天ddd
+        var date = new Date(parseInt(date));
+        return (
+          date.getFullYear() +
+          "/" +
+          (date.getMonth() + 1) +
+          "/" +
+          date.getDate()
+        );
+      }
+    },
     addRoute1() {
       this.$router.push("./Info");
     },
-    addRoute2() {},
+    addRoute2() {
+      this.$router.push("./comment");
+    },
     open2() {
       this.$confirm("返回主界面?", "退出", {
         confirmButtonText: "确定",
--- front/src/pages/others.vue
+++ front/src/pages/others.vue
@@ -1,4 +1,4 @@
-+<template>
+<template>
   <q-page class="flex flex-center">
     <div style="box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)">
       <el-container style="width: 400px">
@@ -14,13 +14,13 @@
             <el-avatar
               shape="circle"
               :size="size"
-              :src="post.header"
+              :src="this.profile_url"
               class="header-img"
               @click.native="addRoute1"
             ></el-avatar>
             <div class="post-info">
-              <span class="author-name">{{ post.authorName }}</span>
-              <span class="author-time">{{ post.id }}</span>
+              <span class="author-name">{{ this.user_name }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+              <span class="author-time">{{ this.user_id }}</span>
             </div>
 
 
@@ -29,7 +29,7 @@
                 size="medium"
                 round
                 class="el-icon-close"
-                @click="addRoute2"
+                @click="open2"
               ></el-button>
             </div>
           </div>
@@ -41,7 +41,7 @@
         <el-input
           type="textarea"
           style="width: 400px"
-          placeholder="简单介绍一下你自己吧"
+          placeholder="this.show_yourself"
           v-model="textarea"
           maxlength="50"
           show-word-limit
@@ -75,22 +75,56 @@
 </template>
 
 <script>
+import Axios from "axios";
+import { axiosInstance } from "../boot/axios.js";
+const clickoutside = {
+  // 初始化指令
+  bind(el, binding, node) {
+    function documentHandler(e) {
+      // 这里判断点击的元素是否是本身，是本身，则返回
+      if (el.contains(e.target)) {
+        return false;
+      }
+      // 判断指令中是否绑定了函数
+      if (binding.expression) {
+        // 如果绑定了函数 则调用那个函数，此处binding.value就是handleClose方法
+        binding.value(e);
+      }
+    }
+    // 给当前元素绑定个私有变量，方便在unbind中可以解除事件监听
+    el.vueClickOutside = documentHandler;
+    document.addEventListener("click", documentHandler);
+  },
+  update() {},
+  unbind(el, binding) {
+    // 解除事件监听
+    document.removeEventListener("click", el.vueClickOutside);
+    delete el.vueClickOutside;
+  },
+};
+
 export default {
-  name: "others",
+  name: "myself",
+  props: {
+    token: String,
+    user_id: Number,
+    user_name: String,
+    profile_url: String,
+    post_id:String,
+    post_time:String,
+  },
   data() {
     return {
-      post: {
-        header: "",
-        authorName: "zzh",
-        id: "00000000",
-        likes: "28",
-        flg: true,
-        pictureUrl:
-          "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
-        previewUrl: [
-          "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
-        ],
-      },
+      user: [],
+      value1: true,
+      btnShow: false,
+      index: "0",
+      post_flg: true,
+      to: "",
+      toId: -1,
+      post: [],
+      replyComment: "",
+      
       activities: [{
           content: '上传了帖子',
           timestamp: '2022-01-01 20:46',
@@ -109,28 +143,68 @@ export default {
           timestamp: '2018-04-03 20:46',
           size: 'large',
         }],
-      value2: [0, 360],
-      value3: 1,
-      textarea: "",
-      input1: "",
-      input2: "",
-      options: [
-        {
-          label: "/static/img/rel/right.png",
-          value1: "0",
-        },
-        {
-          label: "/static/img/rel/left.png",
-          value1: "1",
-        },
-      ],
+ 
     };
   },
   methods: {
+    Refresh: function () {
+      this.Refresh();
+
+      Axios.get("http://127.0.0.1:8000/pages/user_detail/", {
+        params: {
+          token: this.token,
+        },
+      })
+        .then((res) => {
+          console.log(res);
+          this.post = res.data["posts"];
+          this.user = res.data["user"];
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    
+    dateStr(date) {
+      //获取js 时间戳
+      var time = new Date().getTime();
+      //去掉 js 时间戳后三位，与php 时间戳保持一致
+      time = parseInt((time - date) / 1000);
+      //存储转换值
+      var s;
+      if (time < 60 * 10) {
+        //十分钟内
+        return "刚刚";
+      } else if (time < 60 * 60 && time >= 60 * 10) {
+        //超过十分钟少于1小时
+        s = Math.floor(time / 60);
+        return s + "分钟前";
+      } else if (time < 60 * 60 * 24 && time >= 60 * 60) {
+        //超过1小时少于24小时
+        s = Math.floor(time / 60 / 60);
+        return s + "小时前";
+      } else if (time < 60 * 60 * 24 * 30 && time >= 60 * 60 * 24) {
+        //超过1天少于30天内
+        s = Math.floor(time / 60 / 60 / 24);
+        return s + "天前";
+      } else {
+        //超过30天ddd
+        var date = new Date(parseInt(date));
+        return (
+          date.getFullYear() +
+          "/" +
+          (date.getMonth() + 1) +
+          "/" +
+          date.getDate()
+        );
+      }
+    },
     addRoute1() {
       this.$router.push("./Info");
     },
-    addRoute2() {},
+    addRoute2() {
+      this.$router.push("./comment");
+    },
     open2() {
       this.$confirm("返回主界面?", "退出", {
         confirmButtonText: "确定",
